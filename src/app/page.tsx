aca4c7a1afcd0ee0585690c3d4b7b237f337875a
Savotage904 <savolditaddeo@gmail.com>
2025-12-02 17:40:43 +0100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Creative Technologist</h1>
+      <p>Work in progress...</p>
+    </main>
+  );
+}
